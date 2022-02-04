8d3b0e16de61003d5927d4628d0ffd239e1d9d98
YPChen <cyphuaenng@pm.me>
2022-02-05 01:44:11 +0800

    set up Webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.[hash].bundle.js',
+        clean: true,
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        }
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/base.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.[hash].css'
+        })
+    ],
+};
